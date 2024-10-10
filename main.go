3a4aa7378142a1de5088f6cbe4d9145bef9f9986
Sotaneum <gnyontu39@gmail.com>
2024-10-10 21:22:55 +0900

    feat. 인증 모듈 추가

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	parser "github.com/Sotaneum/go-args-parser"
 	"github.com/gin-gonic/gin"
 	ginsession "github.com/go-session/gin-session"
+	"github.com/schedule-job/schedule-job-server/internal/oauth"
 	"github.com/schedule-job/schedule-job-server/internal/pg"
 )
 
@@ -62,6 +64,53 @@ func main() {
 		trustedProxies := strings.Split(options.TrustedProxies, ",")
 		router.SetTrustedProxies(trustedProxies)
 	}
+
+	router.GET("/auth/:name/login", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		url, err := oauth.Core.GetLoginUrl(name)
+
+		if err != nil {
+			ctx.Redirect(302, "/404")
+			return
+		}
+
+		ctx.Redirect(302, url)
+	})
+
+	router.GET("/auth/:name/callback", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		user, userErr := oauth.Core.GetUser(name, ctx.Query("code"))
+		if userErr != nil {
+			ctx.AbortWithError(500, userErr)
+			return
+		}
+
+		store := ginsession.FromContext(ctx)
+		store.Set("userName", user.Name)
+		store.Set("userEmail", user.Email)
+
+		storeErr := store.Save()
+
+		if storeErr != nil {
+			ctx.AbortWithError(500, storeErr)
+			return
+		}
+
+		ctx.Redirect(302, "/")
+	})
+
+	router.GET("/auth/providers", func(ctx *gin.Context) {
+		providers, err := oauth.Core.GetProviders()
+
+		if err != nil {
+			ctx.AbortWithError(500, err)
+			return
+		}
+
+		encoder := json.NewEncoder(ctx.Writer)
+		encoder.SetEscapeHTML(false)
+		encoder.Encode(gin.H{"code": 200, "data": providers})
+	})
 
 	router.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(404, gin.H{"code": 404, "message": "접근 할 수 없는 페이지입니다!"})
